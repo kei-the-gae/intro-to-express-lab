98e956b072638ef1f2b5bdefa0729c6a0237a56b
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-22 15:53:32 -0700

    Add exercise 3

--- server.js
+++ server.js
@@ -20,5 +20,19 @@ app.get('/roll/:number', (req, res) => {
     };
 });
 
+app.get('/collectibles/:index', (req, res) => {
+    const collectibles = [
+        { name: 'shiny ball', price: 5.95 },
+        { name: 'autographed picture of a dog', price: 10 },
+        { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+    ];
+
+    if (req.params.index >= 0 && req.params.index < collectibles.length) {
+        res.send(`So, you want the ${collectibles[req.params.index].name}? For ${collectibles[req.params.index].price}, it can be yours!`);
+    } else {
+        res.send('This item is not yet in stock. Check back soon!');
+    };
+});
+
 // listen
 app.listen(3000, () => { console.log('Listening on port 3000') });
