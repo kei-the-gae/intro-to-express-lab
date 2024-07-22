62943ef0329429739357bdc0f5309a9ef7d62891
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-22 15:44:09 -0700

    Add exercise 2

--- server.js
+++ server.js
@@ -10,5 +10,15 @@ app.get('/greetings/:username', (req, res) => {
     res.send(`Hiya, ${req.params.username}!`);
 });
 
+app.get('/roll/:number', (req, res) => {
+    if (isNaN(req.params.number)) {
+        res.send('You must specify a number.');
+    };
+    if (isNaN(req.params.number) !== true) {
+        const random = Math.floor(Math.random() * req.params.number) + 1;
+        res.send(`You rolled a ${random}.`);
+    };
+});
+
 // listen
 app.listen(3000, () => { console.log('Listening on port 3000') });
