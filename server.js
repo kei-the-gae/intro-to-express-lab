d29af1750abb1b62e05cbb131fccfd456a5405a0
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-22 15:17:09 -0700

    Add exercise 1

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ const app = express();
 
 //define routes
 
+app.get('/greetings/:username', (req, res) => {
+    res.send(`Hiya, ${req.params.username}!`);
+});
+
 // listen
 app.listen(3000, () => { console.log('Listening on port 3000') });
