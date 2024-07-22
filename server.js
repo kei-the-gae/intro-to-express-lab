5d3d060757a245010c5ba5455905fb2b71343bd1
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-22 14:53:01 -0700

    Add initial files and setup

---
+++ server.js
@@ -0,0 +1,10 @@
+// import
+const express = require('express');
+
+//create app
+const app = express();
+
+//define routes
+
+// listen
+app.listen(3000, () => { console.log('Listening on port 3000') });
