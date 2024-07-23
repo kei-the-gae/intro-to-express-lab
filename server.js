75fc82ef0acb83843d0bf874e4b99dfa78f2b9cd
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-07-23 15:26:51 -0700

    Add exercise 4

--- server.js
+++ server.js
@@ -33,5 +33,58 @@ app.get('/collectibles/:index', (req, res) => {
     };
 });
 
+app.get('/shoes', (req, res) => {
+    // access query params
+    const minPrice = Number(req.query['min-price']);
+    const maxPrice = Number(req.query['max-price']);
+    const type = req.query.type;
+
+    const shoes = [
+        { name: "Birkenstocks", price: 50, type: "sandal" },
+        { name: "Air Jordans", price: 500, type: "sneaker" },
+        { name: "Air Mahomeses", price: 501, type: "sneaker" },
+        { name: "Utility Boots", price: 20, type: "boot" },
+        { name: "Velcro Sandals", price: 15, type: "sandal" },
+        { name: "Jet Boots", price: 1000, type: "boot" },
+        { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+    ];
+
+    let filteredList = [];
+
+    //using query params
+    if (minPrice) {
+        shoes.forEach((shoe, i) => {
+            if (shoe.price >= minPrice) {
+                if (filteredList.includes(shoe) === false) {
+                    filteredList.push(shoes[i]);
+                };
+            };
+        });
+    };
+    if (maxPrice) {
+        shoes.forEach((shoe, i) => {
+            if (shoe.price <= maxPrice) {
+                if (filteredList.includes(shoe) === false) {
+                    filteredList.push(shoes[i]);
+                };
+
+            };
+        });
+    };
+    if (type) {
+        shoes.forEach((shoe, i) => {
+            if (shoe.type === type) {
+                if (filteredList.includes(shoe) === false) {
+                    filteredList.push(shoes[i]);
+                };
+            };
+        });
+    };
+    if (!minPrice && !maxPrice && !type) {
+        filteredList = shoes;
+    };
+    res.send(filteredList);
+});
+
 // listen
 app.listen(3000, () => { console.log('Listening on port 3000') });
